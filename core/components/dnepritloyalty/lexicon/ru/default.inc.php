1381f1fa2780cf56675e508e7d9e03bf73d99099
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-08-12 18:23:40 +0300

    Add Russian settings UI lexicon for beta2

--- core/components/dnepritloyalty/lexicon/ru/default.inc.php
+++ core/components/dnepritloyalty/lexicon/ru/default.inc.php
@@ -25,4 +25,88 @@ $_lang['dnepritloyalty_adjust'] =
     'Изменить баланс';
 
 $_lang['dnepritloyalty_save'] =
-    'Сохранить';
+    'Сохранить настройки';
+
+$_lang['dnepritloyalty_reload'] =
+    'Обновить';
+
+$_lang['dnepritloyalty_settings_title'] =
+    'Настройка программы лояльности';
+
+$_lang['dnepritloyalty_settings_intro'] =
+    'Здесь задаются правила начисления и списания бонусов, статусы miniShop2 и параметры Lifetime Discount.';
+
+$_lang['dnepritloyalty_settings_general'] =
+    'Основные настройки';
+
+$_lang['dnepritloyalty_settings_earning'] =
+    'Начисление бонусов за заказы';
+
+$_lang['dnepritloyalty_settings_spending'] =
+    'Оплата бонусами';
+
+$_lang['dnepritloyalty_settings_lifetime'] =
+    'Lifetime Discount';
+
+$_lang['dnepritloyalty_settings_orders'] =
+    'Заказы и статусы';
+
+$_lang['dnepritloyalty_settings_enabled'] =
+    'Программа лояльности включена';
+
+$_lang['dnepritloyalty_settings_point_value'] =
+    'Стоимость 1 бонуса';
+
+$_lang['dnepritloyalty_settings_allowed_groups'] =
+    'Группы пользователей';
+
+$_lang['dnepritloyalty_settings_ids_hint'] =
+    'ID через запятую, например: 1,2,5';
+
+$_lang['dnepritloyalty_settings_order_reward_percent'] =
+    'Начислять с оплаченного заказа, %';
+
+$_lang['dnepritloyalty_settings_min_order_for_reward'] =
+    'Минимальная сумма заказа для начисления';
+
+$_lang['dnepritloyalty_settings_reward_statuses'] =
+    'Статусы miniShop2 для начисления бонусов';
+
+$_lang['dnepritloyalty_settings_spend_enabled'] =
+    'Разрешить оплату заказа бонусами';
+
+$_lang['dnepritloyalty_settings_max_spend_percent'] =
+    'Максимальная часть заказа, оплачиваемая бонусами, %';
+
+$_lang['dnepritloyalty_settings_min_spend_points'] =
+    'Минимальное количество бонусов для списания';
+
+$_lang['dnepritloyalty_settings_min_order_for_spend'] =
+    'Минимальная сумма заказа для списания бонусов';
+
+$_lang['dnepritloyalty_settings_discount_min_order'] =
+    'Минимальная сумма текущего заказа для Lifetime Discount';
+
+$_lang['dnepritloyalty_settings_lifetime_statuses'] =
+    'Статусы miniShop2, учитываемые в общей сумме покупок';
+
+$_lang['dnepritloyalty_settings_lifetime_from'] =
+    'Считать покупки начиная с даты';
+
+$_lang['dnepritloyalty_settings_lifetime_to'] =
+    'Считать покупки до даты';
+
+$_lang['dnepritloyalty_settings_sort_order'] =
+    'Порядок строки скидки в итогах заказа';
+
+$_lang['dnepritloyalty_settings_cancel_statuses'] =
+    'Статусы отмены заказа';
+
+$_lang['dnepritloyalty_settings_status_help'] =
+    'ID статусов зависят от конкретного магазина. Пустое поле не подставляет статусы автоматически.';
+
+$_lang['dnepritloyalty_settings_saved'] =
+    'Настройки сохранены.';
+
+$_lang['dnepritloyalty_settings_load_error'] =
+    'Не удалось загрузить настройки программы лояльности.';
